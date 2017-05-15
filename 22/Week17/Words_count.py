afdf393624e99c62694f32fead48056d28cb3d95
Rosen Tenev <rosenjtenev@gmail.com>
2017-05-15 09:14:27 +0300

    Add files via upload

---
+++ 22/Week17/Words_count.py
@@ -0,0 +1,9 @@
+def words_count():
+	words=["royal_giant","Bucata_Losh","royal_giant","gg_ez","royal_giant","Bucata_Losh"]
+	words_counts={}
+	for word in words:
+		print(word_counts)
+		if word in words_count:
+			word_count[word]+=1
+		else:
+			word_counts[word]=1
